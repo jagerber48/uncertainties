316af6052319a85d60281b1e684d95c6aa532fa8
Justin Gerber <justin.gerber@quantinuum.com>
2024-07-20 23:09:06 -0600

    re organize new/umath.py

--- uncertainties/new/umath.py
+++ uncertainties/new/umath.py
@@ -44,6 +44,39 @@ def add_float_funcs_to_ufloat():
         setattr(UFloat, func_name, ufloat_ufunc)
 
 
+ufuncs_umath_dict = {
+    'exp': lambda x: exp(x),
+    'log': lambda x: log(x),
+    'log2': lambda x: log(x, 2),
+    'log10': lambda x: log10(x),
+    'sqrt': lambda x: sqrt(x),
+    'square': lambda x: x**2,
+    'sin': lambda x: sin(x),
+    'cos': lambda x: cos(x),
+    'tan': lambda x: tan(x),
+    'arcsin': lambda x: asin(x),
+    'arccos': lambda x: acos(x),
+    'arctan': lambda x: atan(x),
+    'arctan2': lambda y, x: atan2(y, x),
+    'hypot': lambda x, y: hypot(x, y),
+    'sinh': lambda self: sinh(self),
+    'cosh': lambda self: cosh(self),
+    'tanh': lambda self: tanh(self),
+    'arcsinh': lambda self: asinh(self),
+    'arccosh': lambda self: acosh(self),
+    'arctanh': lambda self: atanh(self),
+    'degrees': lambda self: degrees(self),
+    'radians': lambda self: radians(self),
+    'deg2rad': lambda self: radians(self),
+    'rad2deg': lambda self: degrees(self),
+}
+
+
+def add_ufuncs_to_ufloat():
+    for func_name, func in ufuncs_umath_dict.items():
+        setattr(UFloat, func_name, func)
+
+
 UReal = Union[Real, UFloat]
 
 
@@ -159,36 +192,3 @@ def add_math_funcs_to_umath():
         func = getattr(math, func_name)
         ufunc = ToUFuncPositional(deriv_funcs, eval_locals={"math": math})(func)
         setattr(this_module, func_name, ufunc)
-
-
-ufuncs_umath_dict = {
-    'exp': lambda x: exp(x),
-    'log': lambda x: log(x),
-    'log2': lambda x: log(x, 2),
-    'log10': lambda x: log10(x),
-    'sqrt': lambda x: sqrt(x),
-    'square': lambda x: x**2,
-    'sin': lambda x: sin(x),
-    'cos': lambda x: cos(x),
-    'tan': lambda x: tan(x),
-    'arcsin': lambda x: asin(x),
-    'arccos': lambda x: acos(x),
-    'arctan': lambda x: atan(x),
-    'arctan2': lambda y, x: atan2(y, x),
-    'hypot': lambda x, y: hypot(x, y),
-    'sinh': lambda self: sinh(self),
-    'cosh': lambda self: cosh(self),
-    'tanh': lambda self: tanh(self),
-    'arcsinh': lambda self: asinh(self),
-    'arccosh': lambda self: acosh(self),
-    'arctanh': lambda self: atanh(self),
-    'degrees': lambda self: degrees(self),
-    'radians': lambda self: radians(self),
-    'deg2rad': lambda self: radians(self),
-    'rad2deg': lambda self: degrees(self),
-}
-
-
-def add_ufuncs_to_ufloat():
-    for func_name, func in ufuncs_umath_dict.items():
-        setattr(UFloat, func_name, func)
