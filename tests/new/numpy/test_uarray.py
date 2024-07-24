b9e254cb9957ef5b3434d7ed55b8acb19914e08f
Justin Gerber <justin.gerber@quantinuum.com>
2024-07-24 17:18:37 -0600

    some UArray tests

--- tests/new/numpy/test_uarray.py
+++ tests/new/numpy/test_uarray.py
@@ -38,11 +38,49 @@ def test_uarray_from_ufloats():
     assert np.all(uarr_1 == uarr_2)
 
 
-def test_binary_ops():
-    uarr = UArray.from_arrays([1, 2, 3], [0.1, 0.2, 0.3])
+def test_uarray_scalar_ops():
+    val_arr = np.array([1, 2, 3])
+    unc_arr = np.array([0.1, 0.2, 0.3])
+    scalar = 42.314
 
-    assert np.all((uarr + uarr).n == np.array([2, 4, 6]))
-    assert np.all((uarr + uarr).expanded_uncertainty == (2 * uarr).expanded_uncertainty)
+    uarr = UArray.from_arrays(val_arr, unc_arr)
 
-    narr = np.array([10, 20, 30])
+    # Check __mul__
+    assert np.all((uarr*scalar).n == val_arr*scalar)
+    assert np.all((uarr*scalar).s == unc_arr*scalar)
 
+    # Check __rmul__
+    assert np.all((scalar*uarr).n == scalar*val_arr)
+    assert np.all((scalar*uarr).s == scalar*unc_arr)
+
+
+def test_uarray_ndarray_ops():
+    val_arr = np.array([1, 2, 3])
+    unc_arr = np.array([0.1, 0.2, 0.3])
+    nd_arr = np.array([10, 20, 30])
+
+    uarr = UArray.from_arrays(val_arr, unc_arr)
+
+    # Check __add__
+    assert np.all((uarr+nd_arr).n == val_arr+nd_arr)
+    assert np.all((uarr+nd_arr).s == unc_arr)
+
+    # Check __radd__
+    assert np.all((nd_arr+uarr).n == nd_arr+val_arr)
+    assert np.all((nd_arr+uarr).s == unc_arr)
+
+
+def test_uarray_uarray_ops():
+    val_arr_1 = np.array([1, 2, 3])
+    unc_arr_1 = np.array([0.1, 0.2, 0.3])
+    uarr_1 = UArray.from_arrays(val_arr_1, unc_arr_1)
+
+    val_arr_2 = np.array([10, 20, 30])
+    unc_arr_2 = np.array([1, 2, 3])
+    uarr_2 = UArray.from_arrays(val_arr_2, unc_arr_2)
+
+    assert np.all((uarr_1+uarr_2).n == val_arr_1+val_arr_2)
+    assert np.all((uarr_1+uarr_2).s == np.sqrt(unc_arr_1**2 + unc_arr_2**2))
+
+    assert np.all((uarr_1-uarr_1).n == np.zeros_like(uarr_1))
+    assert np.all((uarr_1-uarr_1).s == np.zeros_like(uarr_1))
