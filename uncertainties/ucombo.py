c2f6bbabb015ca3802328c78c0d148d6d9d3fcda
Justin Gerber <justin.gerber@quantinuum.com>
2024-07-24 20:41:03 -0600

    commont and typing

--- uncertainties/ucombo.py
+++ uncertainties/ucombo.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 from dataclasses import dataclass, field
 from math import sqrt
 from numbers import Real
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple, TypeVar, Union
 import uuid
 
 
@@ -22,7 +22,7 @@ UComboDict = Dict[UAtom, float]
 
 
 @lru_cache(maxsize=None)
-def get_ucombo_dict(ucombo: UCombo) -> UComboDict:
+def get_ucombo_dict(ucombo: Self) -> UComboDict:
     expanded_dict: UComboDict = defaultdict(float)
 
     for term, term_weight in ucombo:
@@ -44,27 +44,33 @@ def get_std_dev(ucombo_dict: UComboDict) -> float:
     return sqrt(sum(weight**2 for weight in ucombo_dict.values()))
 
 
+Self = TypeVar("Self", bound="UCombo")  # TODO: typing.Self introduced in Python 3.11
+
+
+# TODO: Right now UCombo lacks __slots__. Python 3.10 allows slot=True input argument to
+#   dataclass. Until then the easiest way to get __slots__ back would be to not use a
+#   dataclass here.
 @dataclass(frozen=True)
 class UCombo:
     ucombo_tuple: Tuple[Tuple[Union[UAtom, UCombo], float], ...]
 
     @property
-    def expanded_dict(self: UCombo) -> UComboDict:
+    def expanded_dict(self: Self) -> UComboDict:
         return get_ucombo_dict(self)
 
     @property
-    def std_dev(self: UCombo) -> float:
+    def std_dev(self: Self) -> float:
         return get_std_dev(self.expanded_dict)
 
-    def __add__(self: UCombo, other) -> UCombo:
+    def __add__(self: Self, other) -> Self:
         if not isinstance(other, UCombo):
             return NotImplemented
         return UCombo(((self, 1.0), (other, 1.0)))
 
-    def __radd__(self: UCombo, other):
+    def __radd__(self: Self, other):
         return self.__add__(other)
 
-    def __mul__(self: UCombo, scalar: Real):
+    def __mul__(self: Self, scalar: Real):
         if not isinstance(scalar, Real):
             return NotImplemented
         return UCombo(
@@ -73,13 +79,13 @@ class UCombo:
             )
         )
 
-    def __rmul__(self: UCombo, scalar: Real):
+    def __rmul__(self: Self, scalar: Real):
         return self.__mul__(scalar)
 
-    def __iter__(self):
+    def __iter__(self: Self):
         return iter(self.ucombo_tuple)
 
-    def __str__(self):
+    def __str__(self: Self) -> str:
         ret_str = ""
         first = True
         for term, weight in self:
