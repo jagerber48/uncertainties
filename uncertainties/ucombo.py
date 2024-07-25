028645750c62b527e3ac8104f5756ed1c459bf18
Justin Gerber <justin.gerber@quantinuum.com>
2024-07-24 23:21:21 -0600

    cached_property instead of lru_cache

--- uncertainties/ucombo.py
+++ uncertainties/ucombo.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from functools import lru_cache
+from functools import cached_property
 from dataclasses import dataclass, field
 from math import sqrt
 from numbers import Real
@@ -23,32 +23,6 @@ class UAtom:
         return f"{self.__class__.__name__}({label})"
 
 
-UComboDict = Dict[UAtom, float]
-
-
-@lru_cache(maxsize=None)
-def get_ucombo_dict(ucombo: Self) -> UComboDict:
-    expanded_dict: UComboDict = defaultdict(float)
-
-    for term, term_weight in ucombo:
-        if isinstance(term, UAtom):
-            expanded_dict[term] += term_weight
-        else:
-            expanded_term = get_ucombo_dict(term)
-            for atom, atom_weight in expanded_term.items():
-                expanded_dict[atom] += term_weight * atom_weight
-
-    pruned_expanded_dict = {
-        atom: weight for atom, weight in expanded_dict.items() if weight != 0
-    }
-    return pruned_expanded_dict
-
-
-@lru_cache(maxsize=None)
-def get_std_dev(ucombo_dict: UComboDict) -> float:
-    return sqrt(sum(weight**2 for weight in ucombo_dict.values()))
-
-
 Self = TypeVar("Self", bound="UCombo")  # TODO: typing.Self introduced in Python 3.11
 
 
@@ -59,13 +33,28 @@ Self = TypeVar("Self", bound="UCombo")  # TODO: typing.Self introduced in Python
 class UCombo:
     ucombo_tuple: Tuple[Tuple[Union[UAtom, UCombo], float], ...]
 
-    @property
-    def expanded_dict(self: Self) -> UComboDict:
-        return get_ucombo_dict(self)
+    # TODO: Using cached_property instead of lru_cache. This misses the opportunity to
+    #   cache across separate instances.
+    @cached_property
+    def expanded_dict(self: Self) -> Dict[UAtom, float]:
+        expanded_dict: Dict[UAtom, float] = defaultdict(float)
 
-    @property
+        for term, term_weight in self:
+            if isinstance(term, UAtom):
+                expanded_dict[term] += term_weight
+            else:
+                expanded_term = term.expanded_dict
+                for atom, atom_weight in expanded_term.items():
+                    expanded_dict[atom] += term_weight * atom_weight
+
+        pruned_expanded_dict = {
+            atom: weight for atom, weight in expanded_dict.items() if weight != 0
+        }
+        return pruned_expanded_dict
+
+    @cached_property
     def std_dev(self: Self) -> float:
-        return get_std_dev(self.expanded_dict)
+        return sqrt(sum(weight**2 for weight in self.expanded_dict.values()))
 
     def __add__(self: Self, other) -> Self:
         if not isinstance(other, UCombo):
